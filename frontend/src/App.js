eeeca3df6206572774f4c8daa5ce3a85f9ba8b29
koensantos <95321766+koensantos@users.noreply.github.com>
2025-09-20 19:50:04 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,8 @@ import BillODea from "./pages/BillODea";
 import JoyceWatterman from "./pages/JoyceWatterman";
 import JamesSolomon from "./pages/JamesSolomon";
 import AllCandidatesComparison from "./pages/AllCandidatesComparison";
+import { Analytics } from '@vercel/analytics/next';
+
 
 
 export default function App() {
