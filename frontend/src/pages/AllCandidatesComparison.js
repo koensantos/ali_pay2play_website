a5809648b7f51edfc343f47e426455b83883a691
koensantos <95321766+koensantos@users.noreply.github.com>
2025-09-25 16:10:29 -0400

    changing order of charts

--- frontend/src/pages/AllCandidatesComparison.js
+++ frontend/src/pages/AllCandidatesComparison.js
@@ -19,9 +19,9 @@ import "./DonationComparison.css";
 // Example hardcoded data (replace with your real numbers)
 const candidateData = [
   { name: "Mussab Ali", total: 458408.92, redFlag: 0 },
-  { name: "Bill ODea", total: 1042602.5, redFlag: 307475 },
-  { name: "Jim McGreevey", total: 3614480.39, redFlag: 1258633 },
   { name: "James Solomon", total: 1232474.33, redFlag: 162120 },
+  { name: "Jim McGreevey", total: 3614480.39, redFlag: 1258633 },
+  { name: "Bill ODea", total: 1042602.5, redFlag: 307475 },
   { name: "Joyce Watterman", total: 286641, redFlag: 49815}
 ];
 
