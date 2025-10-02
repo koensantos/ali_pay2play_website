cf9112bc290f9266e77d16f9a5e998a293edf95a
koensantos <95321766+koensantos@users.noreply.github.com>
2025-10-01 23:11:19 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { HashRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import Home from "./pages/Home";
 import JimMcGreevey from "./pages/JimMcGreevey";
 import MussabAli from "./pages/MussabAli";
@@ -8,23 +9,39 @@ import JoyceWatterman from "./pages/JoyceWatterman";
 import JamesSolomon from "./pages/JamesSolomon";
 import AllCandidatesComparison from "./pages/AllCandidatesComparison";
 
+// SPA pageview tracking for React Router
+function AnalyticsTracker() {
+  const location = useLocation();
 
+  useEffect(() => {
+    if (window.__VERCEL_ANALYTICS__) {
+      window.__VERCEL_ANALYTICS__.page();
+    }
+  }, [location]);
 
+  return null; // this component renders nothing
+}
 
 export default function App() {
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/JimMcGreevey" element={<JimMcGreevey />} />
-        <Route path="/MussabAli" element={<MussabAli />} />
-        <Route path="/BillODea" element={<BillODea />} />
-        <Route path="/JoyceWatterman" element={<JoyceWatterman />} />
-        <Route path="/JamesSolomon" element={<JamesSolomon />} />
-        <Route path="/comparison" element={<AllCandidatesComparison />} />
-      </Routes>
-    </Router>
+      <Router>
+        {/* Track SPA route changes */}
+        <AnalyticsTracker />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/JimMcGreevey" element={<JimMcGreevey />} />
+          <Route path="/MussabAli" element={<MussabAli />} />
+          <Route path="/BillODea" element={<BillODea />} />
+          <Route path="/JoyceWatterman" element={<JoyceWatterman />} />
+          <Route path="/JamesSolomon" element={<JamesSolomon />} />
+          <Route path="/comparison" element={<AllCandidatesComparison />} />
+        </Routes>
+
+        {/* Vercel Analytics component */}
+        <Analytics />
+      </Router>
     </>
   );
 }
