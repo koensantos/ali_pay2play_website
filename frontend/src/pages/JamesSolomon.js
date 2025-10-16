1693af7fe75d1fe3febfc893842637565a08f731
koensantos <95321766+koensantos@users.noreply.github.com>
2025-10-16 00:14:57 -0400

    Updating data for red flag percentage.

--- frontend/src/pages/JamesSolomon.js
+++ frontend/src/pages/JamesSolomon.js
@@ -202,7 +202,7 @@ export default function Draft() {
 
       <div className="red-flag-warning">
         <p>
-          This candidate has been flagged for having suspicious donations, totaling to $124,720.{" "}
+          This candidate has been flagged for having suspicious donations, totaling to $159,520.{" "}
           <HashLink smooth to="#red-flags">Click here to view them.</HashLink>
         </p>
       </div>
